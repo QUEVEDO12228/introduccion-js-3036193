04e2710919a973b2e04ba21a8734e6563527e4fd
emanuel <XxEmmaTheCrackxX@gmail.com>
2025-08-19 16:05:13 -0500

    declaración de variables y tipo de dato string

---
+++ 01-variable.js
@@ -0,0 +1,51 @@
+// alert("Hello world")
+// ======================================
+// CONVENCIONES DE NOMBRE PARA CODIFICAR 
+// ======================================
+
+// =============================================================================================
+// UpperCamelCase       => En JS se usa para nombrar las clases.
+// =============================================================================================
+// LowerCamelCase       => En JS se usa Para nombrar las variables.
+// =============================================================================================
+// SCREAMING_SNAKE_CASE => En JS se usa Para nombrar las constantes. 
+// =============================================================================================
+// Snake_Case           => En JS generalmente no se usa, esta convención es muy usada en python.   
+// =============================================================================================
+// caja-de-kebab        => Se usa para nombrar las imagenes los repositorios 
+// =============================================================================================
+// caja-De-tren         => Convencion
+// =============================================================================================
+
+//
+// VARIABLES
+// 1. Ya no se declara una variable con var.
+// 2. En JS No es obligatorio terminar una sentencia con ;. 
+// 3. En js No es obligatorio indicar el tipo de variables.
+
+// como iniciar para declarar una variable:
+// === var product = "sensor-1" ===;
+// === var -sensor = "sensor-2"; === // No se puede iniciar una variable con - al medio
+// === 1sensor = "sensor-3"; ===    // No se puede iniciar una variable con números
+// === var _sensor = "sensor-4"; === // Si es permitido iniciar una variable con _ guion al piso
+
+// Crear una variable con las 3 formas
+
+var sensor = "sensor-lluvia";
+var sensor = "sensor-temperatura";
+var sensor = "sensor-presion";
+console.log(sensor);
+
+let userName = "Emanuel";
+// let userName = "patricia";
+console.log(userName);
+
+// Ejemplo con const 
+// No puedo cambiarle el valor a una variable constante 
+const mazSize = 1024;
+// maxSize = 124568;
+
+// Ejemplo con var
+console.log(userName2);
+// var userName2 = "Quevedo"
+let userName2 = "Quevedo"
